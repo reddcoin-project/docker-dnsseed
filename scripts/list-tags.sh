aa47201fc0cc8db3988bf4aaf7301d0b11a94e80
John Nash <john@redd.ink>
2024-07-17 11:00:27 +1000

    add scripts to aid in uploading/deploying

---
+++ scripts/list-tags.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+#
+## This script returns all lnd tags sorted newest to oldest, with all variants of the same version on the same line
+#
+
+main() {
+  declare repo="$1"
+
+  curl -s "https://hub.docker.com/v2/repositories/$repo/tags" \
+    | jq -r '.results|.[]|.name' \
+    | grep '^v.*' \
+    | grep -v '\-build' \
+    | tr -s '-' '~' \
+    | sort -Vr | uniq \
+    | tr -s '~' '-' \
+    | grep -v '\-\(arm32\|arm64\|amd64\|linux-arm\)' \
+    | awk -F- 'NR==1 {printf "* "}; $1!=a && NR>1 {print "\n* "}; {ORS=""; printf "`%s` ", $0}; {a=$1}'
+
+    echo
+}
+
+main "reddcoincore/dnsseed"
